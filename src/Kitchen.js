3453899bbc22dfddd128e01ec159fe9459e5db1d
Austin Friesorger <aafrieso@svsu.edu>
2023-01-23 18:34:40 -0500

    add component files

---
+++ src/Kitchen.js
@@ -0,0 +1,12 @@
+// CodeSandbox.js
+
+function CodeSandbox(props) {
+  return (
+    <div>
+      <h1>CodeSandbox</h1>
+    </div>
+  );
+}
+
+// Must export the component's function (or class)
+export default CodeSandbox;
